60c7389bed6edf8fabcbb6c4122ec029ac47faca
lewis1190 <lewiskos@hotmail.co.uk>
2025-10-30 20:15:24 +0000

    Compressed mental health images into webps for performance. Updated navbar js code due to loose checking and errors.

--- js/close-navbar-when-clicked.js
+++ js/close-navbar-when-clicked.js
@@ -1,15 +1,17 @@
 // <!-- JavaScript code to ensure the Bootstrap mobile navbar collapses when navigating to in-page links -->
 document.querySelectorAll('.navbar-collapse .nav-link').forEach((link) => {
     link.addEventListener('click', function (e) {
-        let section = document.querySelector(e.target.getAttribute('href'));
-        if (section) {
-            e.preventDefault(); // Prevent default anchor click behavior
-            let navbarHeight = document.querySelector('.navbar-toggler').offsetHeight;
-            window.scroll({
-                top: section.offsetTop - navbarHeight, // Adjust for navbar height
-                behavior: 'smooth'
-            });
-            document.querySelector('.navbar-collapse').classList.remove('show'); // Collapse navbar
+        let href = e.target.getAttribute('href');
+        if (href && href.startsWith('#')) {
+            let section = document.querySelector(href);
+            if (section) {
+                e.preventDefault(); // Prevent default anchor click behavior
+                let navbarHeight = document.querySelector('.navbar-toggler').offsetHeight;
+                window.scroll({});
+                document.querySelector('.navbar-collapse').classList.remove('show'); // Collapse navbar
+            }
+        } else {
+            document.querySelector('.navbar-collapse').classList.remove('show'); // Collapse navbar for external links
         }
     });
 });
